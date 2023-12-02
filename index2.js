3e90cc612f4d4d6d34121986764cc13fe4cf6e56
sahil-m04 <sahilmehta2703@gmail.com>
2023-12-02 21:28:28 +0530

    updated scoreboard

---
+++ index2.js
@@ -0,0 +1,16 @@
+let scoreH = 0;
+let hScore=document.getElementById("score-h")
+function incrementH(){
+    scoreH += 1; 
+    hScore.textContent=scoreH
+}
+
+function saveH() {
+    
+}
+let scoreG = 0
+let gScore=document.getElementById("score-g")
+function incrementG(){
+    scoreG += 1; 
+    gScore.textContent=scoreG
+}
