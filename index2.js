80372fd86ee045657a2d1fe250d2fb20af02ebf9
sahil-m04 <sahilmehta2703@gmail.com>
2023-12-02 23:30:21 +0530

    change in the project

--- index2.js
+++ index2.js
@@ -1,16 +1,18 @@
-let scoreH = 0;
+let scoreH = 0
 let hScore=document.getElementById("score-h")
 function incrementH(){
-    scoreH += 1; 
+    scoreH += 1
     hScore.textContent=scoreH
 }
-
 function saveH() {
     
 }
 let scoreG = 0
 let gScore=document.getElementById("score-g")
 function incrementG(){
-    scoreG += 1; 
+    scoreG += 1 
     gScore.textContent=scoreG
+}
+function saveG() {
+
 }
