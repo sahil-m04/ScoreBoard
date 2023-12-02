4f955abe44706634fa9a9960b5fd0c52b9dcc924
sahil-m04 <sahilmehta2703@gmail.com>
2023-12-03 00:15:40 +0530

    finalcommit

--- index2.js
+++ index2.js
@@ -1,18 +1,14 @@
+let hScore = document.getElementById("score-h")
 let scoreH = 0
-let hScore=document.getElementById("score-h")
 function incrementH(){
     scoreH += 1
     hScore.textContent=scoreH
 }
-function saveH() {
-    
-}
+
+
 let scoreG = 0
 let gScore=document.getElementById("score-g")
 function incrementG(){
     scoreG += 1 
     gScore.textContent=scoreG
-}
-function saveG() {
-
 }
