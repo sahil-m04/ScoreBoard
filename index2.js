a1b32a5322ce65132f90b224a2e30a1b1d50efc3
sahil-m04 <sahilmehta2703@gmail.com>
2023-12-06 01:17:46 +0530

    Made some changes in styling

--- index2.js
+++ index2.js
@@ -1,14 +1,29 @@
 let hScore = document.getElementById("score-h")
 let scoreH = 0
-function incrementH(){
+function incrementH1(){
     scoreH += 1
     hScore.textContent=scoreH
 }
-
+function incrementH2(){
+    scoreH += 2
+    hScore.textContent=scoreH
+}
+function incrementH3(){
+    scoreH += 3
+    hScore.textContent=scoreH
+}
 
 let scoreG = 0
 let gScore=document.getElementById("score-g")
-function incrementG(){
+function incrementG1(){
     scoreG += 1 
+    gScore.textContent=scoreG
+}
+function incrementG2(){
+    scoreG += 2 
+    gScore.textContent=scoreG
+}
+function incrementG3(){
+    scoreG += 3 
     gScore.textContent=scoreG
 }
